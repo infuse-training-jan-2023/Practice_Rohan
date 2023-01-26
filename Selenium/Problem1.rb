ba4ced8a6867bdb0522e61a37f8e39cf6edd583d
2124rohan <vrohan1360@gmail.com>
2023-01-26 11:53:55 +0530

    add url in variable, create object for class and made function name snake case

--- Selenium/Problem1.rb
+++ Selenium/Problem1.rb
@@ -2,14 +2,15 @@ require 'selenium-webdriver'
 Selenium::WebDriver::Chrome.driver_path = "C:\\selenium\\Driver\\chromedriver.exe"
 
 class Problem1 
-    def navigate_to_Browser()
+    def navigate_to_browser()
         driver = Selenium::WebDriver.for :chrome
-
-        driver.get("https://www.google.com/")
+        url = "https://www.google.com/"
+        driver.get(url)
         driver.manage.window.maximize
-        sleep(5)
+        sleep(2)
         driver.quit()
     end
 end
 
-Problem1.new.navigate_to_Browser()
+prob = Problem1.new()
+prob.navigate_to_browser()
