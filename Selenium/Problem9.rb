f293bbf98d5970ee422518434cea391dba8d3c31
2124rohan <vrohan1360@gmail.com>
2023-01-26 13:17:07 +0530

    add url in variable, create object for class, and add made function name snake case

--- Selenium/Problem9.rb
+++ Selenium/Problem9.rb
@@ -2,17 +2,16 @@ require 'selenium-webdriver'
 Selenium::WebDriver::Chrome.driver_path = "C:\\selenium\\Driver\\chromedriver.exe"
 
 class Problem9
-    def get_Table_Header()
+    def get_table_header()
         driver = Selenium::WebDriver.for :chrome
-
-        driver.get("https://cosmocode.io/automation-practice-webtable/")
-
-        trTag = driver.find_elements(:tag_name => 'tr')[0]
-        puts trTag.text
-
+        url = "https://cosmocode.io/automation-practice-webtable/"
+        driver.get(url)
+        get_table_header = driver.find_elements(:tag_name => 'tr')[0]
+        return get_table_header.text
         sleep(2)
         driver.quit()
     end
 end
+prob = Problem9.new()
+puts prob.get_table_header()
 
-Problem9.new.get_Table_Header()
