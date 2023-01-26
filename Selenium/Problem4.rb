b57df4b4d739481a71fa08433a14827fe1f5e15e
2124rohan <vrohan1360@gmail.com>
2023-01-26 12:02:18 +0530

    add url in variable, create object for class and made function name snake case

--- Selenium/Problem4.rb
+++ Selenium/Problem4.rb
@@ -2,18 +2,17 @@ require 'selenium-webdriver'
 Selenium::WebDriver::Chrome.driver_path = "C:\\selenium\\Driver\\chromedriver.exe"
 
 class Problem4
-    def click_Button()
-
+    def click_button()
         driver = Selenium::WebDriver.for :chrome
-
-        driver.get("https://demo.automationtesting.in/Register.html")
+        url ="https://demo.automationtesting.in/Register.html"
+        driver.get(url)
         driver.find_elements(:name, 'radiooptions')[0].click
         driver.find_element(:id, 'checkbox1').click
         driver.find_element(:id, 'checkbox2').click
-
         sleep(2)
         driver.quit()
     end
 end
 
-Problem4.new.click_Button()
+prob = Problem4.new()
+prob.click_button()
