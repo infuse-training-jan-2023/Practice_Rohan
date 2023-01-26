276132acce7c009ca15ec7c081a87d82661d6f2d
2124rohan <vrohan1360@gmail.com>
2023-01-26 13:14:15 +0530

    add url in variable, create object for class, and add arrow keys

--- Selenium/Problem8.rb
+++ Selenium/Problem8.rb
@@ -4,15 +4,19 @@ Selenium::WebDriver::Chrome.driver_path = "C:\\selenium\\Driver\\chromedriver.ex
 class Problem8
   def slider()
     driver = Selenium::WebDriver.for :chrome
-    driver.get("https://www.globalsqa.com/demo-site/sliders/#Steps")
+    url= "https://www.globalsqa.com/demo-site/sliders/#Steps"
+    driver.get(url)
     iframe =driver.find_element(:css,'iframe.lazyloaded')
     driver.switch_to.frame(iframe)
     slider = driver.find_element(:tag_name,'span')
-    driver.action.key_down(:control).perform
-    driver.action.drag_and_drop_by(slider, 100,250).perform
+    left = slider.send_keys(:arrow_left).to_i
+    right = slider.send_keys(:arrow_right).to_i
+    driver.action.drag_and_drop_by(slider, left,right).perform
+    sleep(10)
     driver.quit()
   end
 end
 
-Problem8.new.slider()
+prob = Problem8.new()
+prob.slider()
 
