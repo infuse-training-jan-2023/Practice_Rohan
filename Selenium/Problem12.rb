1c412571eb21e8562b85578e946b51cd7329d536
2124rohan <vrohan1360@gmail.com>
2023-01-26 14:52:35 +0530

    refactor code

--- Selenium/Problem12.rb
+++ Selenium/Problem12.rb
@@ -1,23 +1,28 @@
 require 'selenium-webdriver'
 Selenium::WebDriver::Chrome.driver_path = "C:\\selenium\\Driver\\chromedriver.exe"
-
-driver = Selenium::WebDriver.for :chrome
 class Problem12
-    def navigate_to_Browser_getURL(driver)
+    attr_accessor:driver
+
+    def initialize
+        @driver = Selenium::WebDriver.for :chrome
+    end
+
+    def navigate_to_browser()
         driver.get("https://www.ebay.com/")
         driver.manage.window.maximize
         puts "The URL of the page is: #{driver.current_url}"
+        puts "The title of the page is: #{driver.title}"
     end
 
-    def get_browser_Title(driver)
-        if driver.title != 'Electronics, Cars, Fashion, Collectibles & More | eBay'
-            return "title Not Found"
-        else    
-            return "The title of the page is: #{driver.title}"
-        end
-    end
+    # def get_browser_title()
+    #     if driver.title != 'Electronics, Cars, Fashion, Collectibles & More | eBay'
+    #         return "title Not Found"
+    #     else    
+    #         return "The title of the page is: #{driver.title}"
+    #     end
+    # end
 
-    def search_By_Catergory(driver)
+    def search_by_catergory()
         sel = driver.find_element(:id, 'gh-cat')
         options = sel.find_elements(:tag_name => 'option')
         options.each do |element|
@@ -27,8 +32,8 @@ class Problem12
                 sleep(2)
                 driver.find_element(:id, 's0-27-9-0-1[0]-0-1[0]-0-12-next').click
                 sleep(2)
-
-                aTag = driver.find_element(:xpath,'//*[@id="s0-16-12_2-0-1[0]-0-0"]/ul/li[3]/a').click
+                button = driver.find_elements(:class,'b-textlink--parent')[2]
+                button.click
                 sleep(2)
                 driver.execute_script("window.scrollTo(0, document.body.scrollHeight)")
                 sleep(2)
@@ -41,7 +46,7 @@ class Problem12
         end
     end
 
-    def get_All_categories_Details(driver)
+    def get_all_categories_details()
         divTag = driver.find_element(:id, 'gh-cat-box')
         select = divTag.find_element(:tag_name, 'select')
         options = select.find_elements(:tag_name => 'option')
@@ -50,7 +55,7 @@ class Problem12
         end
     end
 
-    def search_Products(driver)
+    def search_products()
         sel = driver.find_element(:id, 'gh-cat')
         options = sel.find_elements(:tag_name => 'option')
         options.each do |element|
@@ -58,24 +63,24 @@ class Problem12
                 element.click
             end
         end
-
         driver.find_element(:name, '_nkw').send_keys "watch for Men"
         driver.find_element(:id, 'gh-btn').click
         sleep(2)
         container = driver.find_element(:class,'srp-results')
         select_watch = container.find_elements(:tag_name,'li')[20]
-        return select_watch.click
+        select_watch.click
+        sleep(5)
     end
-    
-    def close_browser(driver)
+
+    def close_browser()
         driver.quit()
     end
 end
-prob = Problem12.new()
 
-prob.navigate_to_Browser_getURL(driver)
-puts prob.get_browser_Title(driver)
-puts prob.search_By_Catergory(driver)
-prob.get_All_categories_Details(driver)
-puts prob.search_Products(driver)
-prob.close_browser(driver)
+prob = Problem12.new()
+prob.navigate_to_browser()
+# puts prob.get_browser_title()
+puts prob.search_by_catergory()
+prob.get_all_categories_details()
+prob.search_products()
+prob.close_browser()
