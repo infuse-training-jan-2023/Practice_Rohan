d721cbc3843b042f497d4067f15ec9625dadbd18
2124rohan <vrohan1360@gmail.com>
2023-01-26 12:04:19 +0530

    add url in variable and create object for class

--- Selenium/Problem5.rb
+++ Selenium/Problem5.rb
@@ -4,14 +4,14 @@ Selenium::WebDriver::Chrome.driver_path = "C:\\selenium\\Driver\\chromedriver.ex
 class Problem5
     def search()
         driver = Selenium::WebDriver.for :chrome
-        driver.get("https://www.youtube.com/")
-
+        url = "https://www.youtube.com/"
+        driver.get(url)
         driver.find_element(:name, 'search_query').send_keys "Ruby"
         driver.find_element(:id, 'search-icon-legacy').click
-
         sleep(2)
         driver.quit()
     end
 end
 
-Problem5.new.search()
+prob = Problem5.new()
+prob.search()
