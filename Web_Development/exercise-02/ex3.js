97bbf3775c5bfff8e12ac8290c60ee7e433ab1ee
2124rohan <vrohan1360@gmail.com>
2023-01-27 20:59:03 +0530

    refactor code in ex2.js file

--- Web_Development/exercise-02/ex3.js
+++
@@ -1,9 +0,0 @@
-const currentTime = () => {
-    const date = new Date();
-    document.getElementById('time').innerHTML = date.getHours() + " : " + date.getMinutes() + " : " + date.getSeconds();
-}
-setInterval(() => {
-    currentTime()
-}, 1000);
-
-currentTime()
