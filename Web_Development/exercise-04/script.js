02c8b2e6b4cd2fedbdc75fd3a84ee47028909669
2124rohan <vrohan1360@gmail.com>
2023-01-28 13:30:46 +0530

    access hobbies as a variable

--- Web_Development/exercise-04/script.js
+++ Web_Development/exercise-04/script.js
@@ -8,10 +8,10 @@ const user = {
 /**
  * Use destructuting to log the following
 */
-
 const printUserProfile = ({name,designation,company,hobbies}=user) => {
+    const [hobbies1,hobbies2,hobbies3] = hobbies
     // Piyush Sharma is a Senior Software Engineer at Infuse Consulting. He likes Reading, Listening to music and Collecting stamps
-    console.log(`${name} is a ${designation} at ${company}. He likes ${hobbies[0]}, ${hobbies[1]} and ${hobbies[2]}`);
+    console.log(`${name} is a ${designation} at ${company}. He likes ${hobbies1}, ${hobbies2} and ${hobbies3}`);
 }
 printUserProfile()
 
