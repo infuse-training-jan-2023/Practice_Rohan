0e62fd4530a301b119841ec0c3b4c2eaebfdc213
2124rohan <vrohan1360@gmail.com>
2023-02-05 18:23:24 +0530

    made the changes

--- Docker_Workshop/test.rb
+++ Docker_Workshop/test.rb
@@ -1,58 +1,149 @@
 require 'test/unit'
-require_relative "peachesandcreamTest"
+require_relative "AutomationFrameWork"
+require_relative 'DriverFile'
 
-class Ecommerce < Test::Unit::TestCase
-    
-    def test_instance
-        assert Peachesand_and_Cream_test.new().instance_of? Peachesand_and_Cream_test
+class FormTest < Test::Unit::TestCase
+
+    def test_get_url
+        url="file:///app/index.html"
+        # url = "http://127.0.0.1:5500/index.html"
+        framework = AutomationFamework.new()
+        assert_equal("file:///app/index.html",framework.navigate_to_browser(url))
     end
 
-    # def test_url
-    #     url='file:///app/index.html'
-    #     assert_equal("The URL of the page is: http://127.0.0.1:5500/index.html",Peachesand_and_Cream_test.new().navigate_to_Browser_getURL(url),"Wrong URL: #{url}")
-    # end
+    def test_get_wrong_url
+        url="file:///app/index.html"
+        # url = "http://127.0.0.1:5500/index.html"
+        framework = AutomationFamework.new()
+        assert_equal("Url not found",framework.navigate_to_browser(''))
+    end
+    
+    def test_get_title
+        driver = SeleniumWebDriver.new.create_driver()
+        framework = AutomationFamework.new()
+        framework.driver.get("file:///app/index.html")
+        # framework.driver.get("http://127.0.0.1:5500/index.html")
+        assert_equal("Student-form",framework.get_browser_title())
+    end
+
+    def test_find_element
+        driver = SeleniumWebDriver.new.create_driver()
+        framework = AutomationFamework.new()
+        framework.driver.get("file:///app/index.html")
+        # framework.driver.get("http://127.0.0.1:5500/index.html")
+        element = framework.find_web_element({:xpath=> '/html/body/h3'})
+        assert_equal("Contact Form",element.text)
+    end
+
+    def test_find_element_not_found
+        driver = SeleniumWebDriver.new.create_driver()
+        framework = AutomationFamework.new()
+        framework.driver.get("file:///app/index.html")
+        # framework.driver.get("http://127.0.0.1:5500/index.html")
+        element = framework.find_web_element({:xpath=> '/html/body/h2'})
+        assert_equal("Unable to locate",element)
+    end
+
+    def test_instance
+        assert AutomationFamework.new().instance_of? AutomationFamework
+    end
 
     def test_maximize_browser
-        url="file:///app/index.html"
-        assert_equal("Maximized Browser",Peachesand_and_Cream_test.new().maximize_browser())
+        assert_equal("Maximized Browser",AutomationFamework.new().maximize_browser())
     end
 
-    def test_enter_Data
-        enterValue = Peachesand_and_Cream_test.new()
-        enterValue.navigate_to_Browser_getURL("file:///app/index.html")  
-        assert_equal("entered",enterValue.enter_Data({:id=> 'fname'},"Rohan"))
+    def test_enter_fname
+        driver = SeleniumWebDriver.new.create_driver()
+        framework = AutomationFamework.new()
+        framework.driver.get("file:///app/index.html")
+        # framework.driver.get("http://127.0.0.1:5500/index.html")
+        assert_equal("entered",framework.enter_Data({:id=> 'fname'},"Rohan"))
     end
 
-    def test_click_button
-        enterValue = Peachesand_and_Cream_test.new()
-        enterValue.navigate_to_Browser_getURL("file:///app/index.html") 
-        element = enterValue.find_web_element({:xpath=> '/html/body/div/form/input[3]'})
-        assert_equal("clicked",enterValue.click_button(element),"Unable to Click") 
+    def test_enter_fname_invalid_identifier
+        driver = SeleniumWebDriver.new.create_driver()
+        framework = AutomationFamework.new()
+        framework.driver.get("file:///app/index.html")
+        # framework.driver.get("http://127.0.0.1:5500/index.html")
+        assert_equal("Unable to Enter Data",framework.enter_Data({:id=> 'name'},"Rohan"))
     end
 
     def test_dropdown
-        enterValue = Peachesand_and_Cream_test.new()
-        enterValue.navigate_to_Browser_getURL("file:///app/index.html") 
-        assert_equal("Found Text",enterValue.dropdown({:name=> 'country'},'Canada'),"Unable to Find") 
+        driver = SeleniumWebDriver.new.create_driver()
+        framework = AutomationFamework.new()
+        framework.driver.get("file:///app/index.html")
+        # framework.driver.get("http://127.0.0.1:5500/index.html")
+        assert_equal("Found Text",framework.dropdown({:name=> 'country'},'Canada'),"Unable to Find") 
+    end
 
+    def test_dropdown_invalid_identifier
+        driver = SeleniumWebDriver.new.create_driver()
+        framework = AutomationFamework.new()
+        framework.driver.get("file:///app/index.html")
+        # framework.driver.get("http://127.0.0.1:5500/index.html")
+        assert_equal("Unable to Find",framework.dropdown({:name=> 'try'},'Canada')) 
     end
 
     def test_sleep
-        enterValue = Peachesand_and_Cream_test.new()
-        enterValue.navigate_to_Browser_getURL("file:///app/index.html") 
-        assert_equal("Unable to Sleep",enterValue.wait(p)) 
+        driver = SeleniumWebDriver.new.create_driver()
+        framework = AutomationFamework.new()
+        framework.driver.get("file:///app/index.html")
+        # framework.driver.get("http://127.0.0.1:5500/index.html")
+        assert_equal("sleep for #{2}",framework.wait(2)) 
     end
 
-    def test_class_element
-        enterValue = Peachesand_and_Cream_test.new()
-        enterValue.navigate_to_Browser_getURL("file:///app/index.html")  
-        assert_equal("entered",enterValue.enter_Data({:class=> 'message'},"hello world"))
+    def test_sleep_invalid_input
+        driver = SeleniumWebDriver.new.create_driver()
+        framework = AutomationFamework.new()
+        framework.driver.get("file:///app/index.html")
+        # framework.driver.get("http://127.0.0.1:5500/index.html")
+        assert_equal("Unable to Sleep",framework.wait(p)) 
     end
 
-    def test_xpath_element
-        enterValue = Peachesand_and_Cream_test.new()
-        enterValue.navigate_to_Browser_getURL("file:///app/index.html")  
-        assert_equal("entered",enterValue.enter_Data({:xpath=> '//*[@id="lname"]'},"hello world"))
+    def test_enter_data_textarea
+        driver = SeleniumWebDriver.new.create_driver()
+        framework = AutomationFamework.new()
+        framework.driver.get("file:///app/index.html")
+        # framework.driver.get("http://127.0.0.1:5500/index.html")
+        assert_equal("entered",framework.enter_Data({:class=> 'message'},"hello world"))
+    end
+
+    def test_enter_data_message_invalid_identifier
+        driver = SeleniumWebDriver.new.create_driver()
+        framework = AutomationFamework.new()
+        framework.driver.get("file:///app/index.html")
+        # framework.driver.get("http://127.0.0.1:5500/index.html")
+        assert_equal("Unable to Enter Data",framework.enter_Data({:class=> 'msage'},"hello world"))
+    end
+
+    def test_enter_data_lname
+        driver = SeleniumWebDriver.new.create_driver()
+        framework = AutomationFamework.new()
+        framework.driver.get("file:///app/index.html")
+        # framework.driver.get("http://127.0.0.1:5500/index.html") 
+        assert_equal("entered",framework.enter_Data({:xpath=> '//*[@id="lname"]'},"hello world"))
+    end
+
+    def test_enter_data_lname_invalid_identifier
+        driver = SeleniumWebDriver.new.create_driver()
+        framework = AutomationFamework.new()
+        framework.driver.get("file:///app/index.html")
+        # framework.driver.get("http://127.0.0.1:5500/index.html") 
+        assert_equal("Unable to Enter Data",framework.enter_Data({:xpath=> '//[@id="lname"]'},"hello world"))
+    end
+
+    def test_submit_form
+        driver = SeleniumWebDriver.new.create_driver()
+        framework = AutomationFamework.new()
+        framework.driver.get("file:///app/index.html")
+        # framework.driver.get("http://127.0.0.1:5500/index.html")
+        framework.driver.find_element(:id=> 'fname').send_keys "rohan"
+        framework.driver.find_element(:id=> 'lname').send_keys "vishwakarma"
+        framework.driver.find_element(:id=> 'subject').send_keys "hello world"
+        element = framework.driver.find_element(:id=> 'submit')
+        assert_equal("Submit",framework.driver.find_element(:id=> 'submit').text) 
+        assert_equal("clicked",framework.click_button(element)) 
+        sleep(2)
     end
 end
 
