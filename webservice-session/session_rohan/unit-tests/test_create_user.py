edcf307fecc7daf9860f41da501f2487962ceeab
2124rohan <vrohan1360@gmail.com>
2023-02-07 09:32:52 +0530

    made changes

--- webservice-session/session_rohan/unit-tests/test_create_user.py
+++ webservice-session/session_rohan/unit-tests/test_create_user.py
@@ -1,15 +1,15 @@
-from src.item_repository import ItemRepository
+from src.user_repository import UserRepository
 
 user = [('rohan', 23, 'Male', 'GOA')]
 
-item_repo = ItemRepository()
+item_repo = UserRepository()
 
 def test_create_new_user_makes_db_call(mocker):
-  mock = mocker.patch('src.item_repository.ItemRepository.add_new_user', return_value = [])
+  mock = mocker.patch('src.user_repository.UserRepository.add_new_user', return_value = [])
   _ = item_repo.add_new_user()
   assert mock.call_count == 1
 
 def test_create_new_user(mocker):
-  mock = mocker.patch('src.item_repository.ItemRepository.add_new_user', return_value = user)
+  mock = mocker.patch('src.user_repository.UserRepository.add_new_user', return_value = user)
   user_returned = item_repo.add_new_user()
   assert user == user_returned
--- webservice-session/session_rohan/unit-tests/test_csv_file.py
+++ webservice-session/session_rohan/unit-tests/test_csv_file.py
@@ -1,15 +1,15 @@
-from src.item_repository import ItemRepository
+from src.item_actions import ItemAction
 
 items = {"status":"Saved to CSV file"}
 
-item_repo = ItemRepository()
+item_repo = ItemAction()
 
 def test_add_item_to_csv_db_call(mocker):
-  mock = mocker.patch('src.item_repository.ItemRepository.add_item', return_value = [])
-  _ = item_repo.add_item()
+  mock = mocker.patch('src.item_actions.ItemAction.add_item', return_value = [])
+  _ = item_repo.add_item_to_csv()
   assert mock.call_count == 1
 
 def test_add_item_to_csv(mocker):
-  mock = mocker.patch('src.item_repository.ItemRepository.add_item', return_value = items)
-  item_returned = item_repo.add_item()
+  mock = mocker.patch('src.item_actions.ItemAction.add_item', return_value = items)
+  item_returned = item_repo.add_item_to_csv()
   assert items == item_returned
